b6b237f8164323808ff3e14fe66a4ed38454c656
heyMP <mgp140@gmail.com>
2021-10-26 21:17:25 -0400

    remove unused ssr env

--- api/index.js
+++ api/index.js
@@ -3,11 +3,9 @@ import { render } from '@lit-labs/ssr/lib/render-with-global-dom-shim.js';
 import { Readable } from 'stream';
 import { html } from 'lit';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
-const PROXY_FQDN = process.env.PROXY_FQDN ?? 'http://localhost:3000';
 import '../rh-footer.js';
 
 export default async function handler(req, res) {
-	// window.location = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
 	window.location = new URL(`${req.headers['x-forwarded-proto']}://${req.headers['x-vercel-deployment-url']}`);
 	const response = await fetch(window.location);
 	const body = await response.text();
