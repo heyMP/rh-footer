76b6b21124cf3146e8decfe7f2dfd0877b6aa6ae
heyMP <mgp140@gmail.com>
2022-03-07 10:08:17 -0500

    adding logo to footer

--- src/RhFooter.ts
+++ src/RhFooter.ts
@@ -92,10 +92,15 @@ export class RhFooter extends LitElement {
           display: grid;
           grid-template-columns: 1fr;
           grid-template-areas:
+            "logo"
             "primary"
             "secondary"
             "tertiary";
           gap: var(--pf-global--spacer--xl, 32px);
+        }
+
+        .footer__logo {
+          grid-area: logo;
         }
 
         .footer__primary {
@@ -118,6 +123,7 @@ export class RhFooter extends LitElement {
           .footer {
             grid-template-columns: 4fr 4fr 4fr;
             grid-template-areas:
+              "logo logo logo"
               "primary primary primary"
               "secondary secondary tertiary";
           }
@@ -143,10 +149,10 @@ export class RhFooter extends LitElement {
           }
 
           .footer {
-            grid-template-columns: 10fr 2fr;
+            grid-template-columns: 42px 10fr 2fr;
             grid-template-areas:
-              "primary tertiary"
-              "secondary tertiary";
+              "logo primary tertiary"
+              "logo secondary tertiary";
           }
         }
 
@@ -179,6 +185,10 @@ export class RhFooter extends LitElement {
         .links rh-footer-links,
         .links slot::slotted(rh-footer-links) {
           width: calc((100% / var(--rh-footer--links-columns, 4)) - var(--pf-global--spacer--xl, 32px));
+        }
+
+        #footer-logo {
+          width: 42px;
         }
       `,
     ];
@@ -313,8 +323,15 @@ export class RhFooter extends LitElement {
           </div>
           <div class="section footer" part="section footer">
             <slot name="footer">
+              <div class="footer__logo" part="footer__logo">
+                <slot name="footer__logo">
+                  <a href="/en" title="Red Hat">
+                    <svg id="footer-logo" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 145"><defs><style>.cls-1{fill:#6A6E73;} .band{fill:transparent;}</style></defs><title>RedHat-Logo-Hat-Color</title><path class="band" d="M157.77,62.61a14,14,0,0,1,.31,3.42c0,14.88-18.1,17.46-30.61,17.46C78.83,83.49,42.53,53.26,42.53,44a6.43,6.43,0,0,1,.22-1.94l-3.66,9.06a18.45,18.45,0,0,0-1.51,7.33c0,18.11,41,45.48,87.74,45.48,20.69,0,36.43-7.76,36.43-21.77,0-1.08,0-1.94-1.73-10.13Z"/><path class="cls-1" d="M127.47,83.49c12.51,0,30.61-2.58,30.61-17.46a14,14,0,0,0-.31-3.42l-7.45-32.36c-1.72-7.12-3.23-10.35-15.73-16.6C124.89,8.69,103.76.5,97.51.5,91.69.5,90,8,83.06,8c-6.68,0-11.64-5.6-17.89-5.6-6,0-9.91,4.09-12.93,12.5,0,0-8.41,23.72-9.49,27.16A6.43,6.43,0,0,0,42.53,44c0,9.22,36.3,39.45,84.94,39.45M160,72.07c1.73,8.19,1.73,9.05,1.73,10.13,0,14-15.74,21.77-36.43,21.77C78.54,104,37.58,76.6,37.58,58.49a18.45,18.45,0,0,1,1.51-7.33C22.27,52,.5,55,.5,74.22c0,31.48,74.59,70.28,133.65,70.28,45.28,0,56.7-20.48,56.7-36.65,0-12.72-11-27.16-30.83-35.78"/></svg>
+                  </a>
+                </slot>
+              </div>
               <div class="footer__primary" part="footer__primary">
-                <slot name="footer__primary"> </slot>
+                <slot name="footer__primary"></slot>
               </div>
               <div class="footer__secondary" part="footer__secondary">
                 <slot name="footer__secondary"></slot>
