d3a44b016a1310e7ae7d56434eb12dbcc6d428a7
heyMP <mgp140@gmail.com>
2022-04-04 20:32:40 -0400

    fix: added aria-labelledby to rh-footer-links

--- src/rh-footer-links.ts
+++ src/rh-footer-links.ts
@@ -1,7 +1,18 @@
 import { LitElement, html, css } from 'lit';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
+import { SlotController } from '@patternfly/pfe-core/controllers/slot-controller';
+import { bound, initializer } from '@patternfly/pfe-core/decorators';
+import { Logger } from '@patternfly/pfe-core/controllers/logger';
+import { getRandomId } from '@patternfly/pfe-core/functions/random';
 
 export class RhFooterLinks extends LitElement {
+
+	private logger = new Logger(this);
+
+	private slots = new SlotController(this, {
+		slots: ['header'],
+	});
+
 	static get styles() {
 		return css`
 			:host {
@@ -20,14 +31,35 @@ export class RhFooterLinks extends LitElement {
 
 	constructor() {
 		super();
+	}
+
+	@initializer()
+	protected _init(records: MutationRecord[]) {
+		this.updateAccessibility();
+	}
+
+	public updateAccessibility() {
 		this.setAttribute('role', 'list');
+		// ensure we've rendered to our shadowroot
+		const header: HTMLElement | null = this.querySelector('[slot="header"]');
+		if (header) {
+			// ensure there is an id on the header slot
+			const headerId = header.id || getRandomId('rh-footer-links');
+			if (headerId !== header.id) header.id = headerId;
+			header.id = headerId;
+			this.setAttribute('aria-labelledby', headerId);
+		}
+		else {
+			// if there is no header then we need to remove aria-labelledby
+			this.removeAttribute('aria-labelledby');
+		}
 	}
 
 	render() {
 		return html`
 			<slot name="header"></slot>
 			<slot></slot>
-		`;
+	`;
 	}
 }
 
