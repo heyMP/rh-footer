b51699b0218e066a0edc3a066303930bb84705f3
heyMP <mgp140@gmail.com>
2022-04-06 10:11:18 -0400

    fix: whitepace

--- src/rh-footer-copyright.ts
+++ src/rh-footer-copyright.ts
@@ -1,26 +1,26 @@
 import { html, css, LitElement } from 'lit';
 
 export class RhFooterCopyright extends LitElement {
-	static get tag() {
-		return 'rh-footer-copyright';
-	}
+  static get tag() {
+    return 'rh-footer-copyright';
+  }
 
-	static get styles() {
-		return css`
+  static get styles() {
+    return css`
 			:host {
 				flex: 1 1 auto;
 				display: block;
 				width: 100%;
 			}
 		`;
-	}
+  }
 
-	render() {
-		const currentYear = new Date().getFullYear();
-		return html`
-			<slot>&copy; ${currentYear} Red Hat, Inc.</slot>
+  render() {
+    const currentYear = new Date().getFullYear();
+    return html`
+      <slot>&copy; ${currentYear} Red Hat, Inc.</slot>
     `;
-	}
+  }
 }
 
 customElements.define(RhFooterCopyright.tag, RhFooterCopyright);
