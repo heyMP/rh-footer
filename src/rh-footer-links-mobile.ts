64bc229cd5c30bc347495215facb0da3ce887d4a
heyMP <mgp140@gmail.com>
2022-04-06 13:55:08 -0400

    chore: formatting

--- src/rh-footer-links-mobile.ts
+++ src/rh-footer-links-mobile.ts
@@ -4,19 +4,19 @@ import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import { mobileBreakpoint } from './lib/tokens.js';
 
 interface LinkSet {
-	header: HTMLElement | null,
-	panel: Element[]
+  header: HTMLElement | null,
+  panel: Element[]
 }
 
 export class RhFooterLinksMobile extends LitElement {
-	static get tag() {
-		return `rh-footer-links-mobile`;
-	}
+  static get tag() {
+    return `rh-footer-links-mobile`;
+  }
 
-	@state() private linkSets?: LinkSet[];
+  @state() private linkSets?: LinkSet[];
 
-	static get styles() {
-		return css`
+  static get styles() {
+    return css`
       :host {
         display: block;
 				width: 100%;
@@ -49,56 +49,56 @@ export class RhFooterLinksMobile extends LitElement {
 				}
 			}
     `;
-	}
+  }
 
-	firstUpdated() {
-		this.build();
-	}
+  firstUpdated() {
+    this.build();
+  }
 
-	async build(): Promise<void> {
-		// get a list of rh-footer-links items
-		if (this.shadowRoot) {
-			const children = this.shadowRoot.querySelector('slot')?.assignedElements({ flatten: true });
-			if (children && children.length > 0) {
-				const linkSets: LinkSet[] | undefined = this.shadowRoot.querySelector('slot')?.assignedElements({ flatten: true })
-					.map(item => ({
-						// for each header we need to create an array of panel items that it's associated with.
-						header: item.querySelector('[slot="header"]'),
-						// collect all of the headers siblings
-						panel: [...item.children]
-							.filter(child => child.getAttribute('slot') !== 'header')
-							.map(child => {
-								// ensure it has a class of .link
-								child.classList.add('link');
-								// ensure it has a part name of link
-								child.setAttribute('part', 'link');
-								return child;
-							}),
-					}));
+  async build(): Promise<void> {
+    // get a list of rh-footer-links items
+    if (this.shadowRoot) {
+      const children = this.shadowRoot.querySelector('slot')?.assignedElements({ flatten: true });
+      if (children && children.length > 0) {
+        const linkSets: LinkSet[] | undefined = this.shadowRoot.querySelector('slot')?.assignedElements({ flatten: true })
+          .map(item => ({
+            // for each header we need to create an array of panel items that it's associated with.
+            header: item.querySelector('[slot="header"]'),
+            // collect all of the headers siblings
+            panel: [...item.children]
+              .filter(child => child.getAttribute('slot') !== 'header')
+              .map(child => {
+                // ensure it has a class of .link
+                child.classList.add('link');
+                // ensure it has a part name of link
+                child.setAttribute('part', 'link');
+                return child;
+              }),
+          }));
 
-				if (linkSets) {
-					this.linkSets = linkSets;
-				}
-			};
-		}
-	}
+        if (linkSets) {
+          this.linkSets = linkSets;
+        }
+      };
+    }
+  }
 
-	render() {
-		return html`
-			<div id="dynamic-links" class="base" part="base">
-				${this.linkSets ? html`
-				<pfe-accordion part="accordion">
-					${this.linkSets?.map(item => html`
-					<pfe-accordion-header part="accordion-header">${unsafeHTML(item.header?.outerHTML)}</pfe-accordion-header>
-					<pfe-accordion-panel part="accordion-panel">${item.panel.map(_item => html`${unsafeHTML(_item.outerHTML)}`)}
-					</pfe-accordion-panel>
-					`)}
-				</pfe-accordion>
-				` : ''}
-			</div>
-			<slot id="default-slot" hidden></slot>
+  render() {
+    return html`
+      <div id="dynamic-links" class="base" part="base">
+        ${this.linkSets ? html`
+        <pfe-accordion part="accordion">
+          ${this.linkSets?.map(item => html`
+          <pfe-accordion-header part="accordion-header">${unsafeHTML(item.header?.outerHTML)}</pfe-accordion-header>
+          <pfe-accordion-panel part="accordion-panel">${item.panel.map(_item => html`${unsafeHTML(_item.outerHTML)}`)}
+          </pfe-accordion-panel>
+          `)}
+        </pfe-accordion>
+        ` : ''}
+      </div>
+      <slot id="default-slot" hidden></slot>
 		`;
-	}
+  }
 }
 
 customElements.define(RhFooterLinksMobile.tag, RhFooterLinksMobile);
